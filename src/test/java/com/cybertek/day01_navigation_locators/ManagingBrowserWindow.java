178c7df9e822fafd2949ddfec4c58b63ea428dcb
Yahya <yahyahussein91@gmail.com>
2021-09-05 15:44:51 -0700

    new class to practice managing broswer window

---
+++ src/test/java/com/cybertek/day01_navigation_locators/ManagingBrowserWindow.java
@@ -0,0 +1,17 @@
+package com.cybertek.day01_navigation_locators;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ManagingBrowserWindow {
+    public static void main(String[] args) {
+
+        //set up chrome driver
+        WebDriverManager.chromedriver().setup();
+        // create chrome driver object
+        WebDriver driver = new ChromeDriver();
+        // navigate to https://yahoo.com
+        driver.get("https://amazon.com");
+    }
+}
