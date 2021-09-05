98895a53dddbe88fbf987bea2a292c173d865fd2
Yahya <yahyahussein91@gmail.com>
2021-09-05 00:06:54 -0700

    set up new chrome driver instance

--- src/test/java/com/cybertek/SeleniumDryRun.java
+++ src/test/java/com/cybertek/SeleniumDryRun.java
@@ -1,15 +1,18 @@
 package com.cybertek;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumDryRun {
     public static void main(String[] args) {
 
         //1. setup chrome driver using webdriver manager
             // so selenium can send interaction request
-        WebDriverManager
+        WebDriverManager.chromedriver().setup();
 
-        //2.
+        //2. create webdriver instance using chrome driver object
+        WebDriver drive = new ChromeDriver();
 
     }
 }
