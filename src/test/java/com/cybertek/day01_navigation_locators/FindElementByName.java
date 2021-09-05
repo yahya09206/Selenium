12a69294454db0b24f8b67d41b9bc14338364dae
Yahya <yahyahussein91@gmail.com>
2021-09-05 16:33:10 -0700

    more element practice

---
+++ src/test/java/com/cybertek/day01_navigation_locators/FindElementByName.java
@@ -0,0 +1,16 @@
+package com.cybertek.day01_navigation_locators;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FindElementByName {
+    public static void main(String[] args) {
+
+        // set up chrome driver using web driver manager
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        //navigate to yahoo.com
+        driver.get("https://yahoo.com");
+    }
+}
