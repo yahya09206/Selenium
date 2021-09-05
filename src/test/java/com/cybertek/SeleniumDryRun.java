102de3f33e33f1b14facb80beb04bda27c735c6a
Yahya <yahyahussein91@gmail.com>
2021-09-05 00:06:10 -0700

    setting up chrome driver manager

---
+++ src/test/java/com/cybertek/SeleniumDryRun.java
@@ -0,0 +1,15 @@
+package com.cybertek;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SeleniumDryRun {
+    public static void main(String[] args) {
+
+        //1. setup chrome driver using webdriver manager
+            // so selenium can send interaction request
+        WebDriverManager
+
+        //2.
+
+    }
+}
