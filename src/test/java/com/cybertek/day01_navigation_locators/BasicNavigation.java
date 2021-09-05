5e376febc47e62751282dc3148fb5f683f919d6c
Yahya <yahyahussein91@gmail.com>
2021-09-05 15:10:35 -0700

    set up instructions

--- src/test/java/com/cybertek/day01_navigation_locators/BasicNavigation.java
+++ src/test/java/com/cybertek/day01_navigation_locators/BasicNavigation.java
@@ -1,4 +1,12 @@
 package com.cybertek.day01_navigation_locators;
 
 public class BasicNavigation {
+    public static void main(String[] args) {
+
+        //set up chrome driver
+
+        // create chromedriver object
+
+        // navigate to https://yahoo.com
+    }
 }
