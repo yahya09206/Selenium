addfd7837cd8fbda1db890af732d3185fda0790f
Yahya <yahyahussein91@gmail.com>
2021-09-05 15:11:13 -0700

    set up chrome driver

--- src/test/java/com/cybertek/day01_navigation_locators/BasicNavigation.java
+++ src/test/java/com/cybertek/day01_navigation_locators/BasicNavigation.java
@@ -1,12 +1,17 @@
 package com.cybertek.day01_navigation_locators;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class BasicNavigation {
     public static void main(String[] args) {
 
         //set up chrome driver
-
+        WebDriverManager.chromedriver().setup();
         // create chromedriver object
-
+        WebDriver driver = new ChromeDriver();
         // navigate to https://yahoo.com
+
     }
 }
