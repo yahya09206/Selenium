98c2ca4acf19f2d29d5246526ac4f3121cb74a5e
Yahya <yahyahussein91@gmail.com>
2021-09-05 16:16:11 -0700

    getting website

--- src/test/java/com/cybertek/day01_navigation_locators/FindElementById.java
+++ src/test/java/com/cybertek/day01_navigation_locators/FindElementById.java
@@ -1,7 +1,17 @@
 package com.cybertek.day01_navigation_locators;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class FindElementById {
     public static void main(String[] args) {
+
+        // set up chrome driver using web driver manager
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        //navigate to yahoo.com
+        driver.get("https://yahoo.com");
 
     }
 }
