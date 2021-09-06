8b8a3e758af34a6c606f4dee15a51807da339705
Yahya <yahyahussein91@gmail.com>
2021-09-05 21:04:29 -0700

    entering name inside box

--- src/test/java/com/cybertek/homework/Homework1.java
+++ src/test/java/com/cybertek/homework/Homework1.java
@@ -1,7 +1,9 @@
 package com.cybertek.homework;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Homework1 {
@@ -10,6 +12,10 @@ public class Homework1 {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("https://facebook.com");
+
+        // identify username and password boxes
+        WebElement username = driver.findElement(By.id("email"));
+        username.sendKeys("invalid username");
 
     }
 }
