36c61992befba31ce9652b9ddf3a6ecf92cf73e1
Yahya <yahyahussein91@gmail.com>
2021-09-05 21:02:13 -0700

    navigating to facebook

--- src/test/java/com/cybertek/homework/Homework1.java
+++ src/test/java/com/cybertek/homework/Homework1.java
@@ -1,7 +1,15 @@
 package com.cybertek.homework;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Homework1 {
     public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://facebook.com");
 
     }
 }
