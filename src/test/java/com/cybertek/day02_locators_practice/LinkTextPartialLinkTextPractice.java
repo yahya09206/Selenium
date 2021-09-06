409dc1760afbcf8753f97c072a68f283ee51bc64
Yahya <yahyahussein91@gmail.com>
2021-09-05 23:03:06 -0700

    links opening as expected

--- src/test/java/com/cybertek/day02_locators_practice/LinkTextPartialLinkTextPractice.java
+++ src/test/java/com/cybertek/day02_locators_practice/LinkTextPartialLinkTextPractice.java
@@ -3,6 +3,7 @@ package com.cybertek.day02_locators_practice;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LinkTextPartialLinkTextPractice {
@@ -26,12 +27,16 @@ public class LinkTextPartialLinkTextPractice {
         driver.manage().window().maximize();
 
         // click on second link, either full linktext or partial link text
-        driver.findElement(By.linkText("Add/Remove Elements")).click();
+        //driver.findElement(By.linkText("Add/Remove Elements")).click();
+        WebElement addOrRemoveLink = driver.findElement(By.partialLinkText("Add/Remove"));
+        addOrRemoveLink.click();
 
 
         // click on home link to go back to home
+        driver.findElement(By.linkText("Home")).click();
 
         // click on redirect link
+        driver.findElement(By.linkText("Redirect Link")).click();
 
         // close browser
         driver.close();
