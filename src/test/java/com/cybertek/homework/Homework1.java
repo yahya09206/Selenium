dca39edd10ad9a499bb77409a1dfb34f5e97ed45
Yahya <yahyahussein91@gmail.com>
2021-09-05 21:26:33 -0700

    added pauses in between, assignment completed

--- src/test/java/com/cybertek/homework/Homework1.java
+++ src/test/java/com/cybertek/homework/Homework1.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Homework1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
@@ -16,6 +16,51 @@ public class Homework1 {
         // identify username and password boxes
         WebElement username = driver.findElement(By.id("email"));
         username.sendKeys("invalid username");
+
+        // password
+        WebElement password = driver.findElement(By.id("pass"));
+        password.sendKeys("some password");
+
+        // click login button
+        WebElement login = driver.findElement(By.name("login"));
+        login.submit();
+
+        // navigate back to login screen
+        driver.navigate().back();
+
+        // pause
+        Thread.sleep(5000);
+
+        // click on forgot password link
+        WebElement forgot = driver.findElement(By.linkText("Forgot Password?"));
+        forgot.click();
+
+        Thread.sleep(5000);
+
+        // not me button
+        WebElement notMe = driver.findElement(By.name("reset_action"));
+        notMe.click();
+
+        Thread.sleep(5000);
+
+        // enter mobile number textbox
+        WebElement phoneNumber = driver.findElement(By.id("identify_email"));
+        phoneNumber.sendKeys("something");
+
+        Thread.sleep(5000);
+
+        // click on search button
+        WebElement search = driver.findElement(By.id("did_submit"));
+        search.click();
+
+        Thread.sleep(5000);
+
+        // click on cancel button
+//        WebElement cancel = driver.findElement(By.linkText("Cancel"));
+//        cancel.click();
+
+        // close window
+        //driver.quit();
 
     }
 }
