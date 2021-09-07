5a6c382642a58e24b015e8e4ea1af61962331dab
Yahya <yahyahussein91@gmail.com>
2021-09-07 13:22:32 -0700

    using relative path to get element

--- src/test/java/com/cybertek/day04_common_elements_xpath_css_intro/FindElementByXPath_Intro.java
+++ src/test/java/com/cybertek/day04_common_elements_xpath_css_intro/FindElementByXPath_Intro.java
@@ -19,5 +19,11 @@ public class FindElementByXPath_Intro {
         // Using above full XPath to identify the element and get the text
         WebElement headerSpan = driver.findElement(By.xpath("/html/body/div/div[2]/div/div/div/h1/span"));
         System.out.println("headerSpan.getText() = " + headerSpan.getText());
+
+        // Use relative path
+        // h1/span : start looking for element with h1 whoever parents or grandparents are
+                    // find direct child with span tag
+        WebElement headerSpan2 = driver.findElement(By.xpath("//h1/span"));
+        System.out.println("headerSpan2.getText() = " + headerSpan2.getText());
     }
 }
