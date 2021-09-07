a6d808671b717f94726c6c5560f66f287f02fb51
Yahya <yahyahussein91@gmail.com>
2021-09-06 22:49:17 -0700

    find element by tag name

--- src/test/java/com/cybertek/day03_locate_by_tag_class_getatt/FindElementByTagName.java
+++ src/test/java/com/cybertek/day03_locate_by_tag_class_getatt/FindElementByTagName.java
@@ -1,7 +1,28 @@
 package com.cybertek.day03_locate_by_tag_class_getatt;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class FindElementByTagName {
     public static void main(String[] args) {
+
+        /*
+         * Purple tag name
+         * Orange attribute name
+         * blue attribute value
+         * black it is the text
+         * */
+
+        // set up driver and open chrome
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://practice.cybertekschool.com/");
+
+        // identify first element with tag name a
+        WebElement firstLink = driver.findElement(By.tagName("a"));
 
     }
 }
