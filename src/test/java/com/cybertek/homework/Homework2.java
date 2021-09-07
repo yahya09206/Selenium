20fce5817c7a83a589eadd8e2f1a230c7b28fcf9
Yahya <yahyahussein91@gmail.com>
2021-09-06 19:38:13 -0700

    set up to get website

--- src/test/java/com/cybertek/homework/Homework2.java
+++ src/test/java/com/cybertek/homework/Homework2.java
@@ -1,7 +1,16 @@
 package com.cybertek.homework;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Homework2 {
     public static void main(String[] args) {
 
+
+        //Set up Chrome Driver
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://zero.webappsecurity.com/");
     }
 }
