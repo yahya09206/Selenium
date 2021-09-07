4b63b52e5cfce9be1964560f743a7a749c288578
Yahya <yahyahussein91@gmail.com>
2021-09-07 09:24:25 -0700

    locating first dropdown

--- src/test/java/com/cybertek/day04_common_elements_xpath_css_intro/LocatingDropDownList.java
+++ src/test/java/com/cybertek/day04_common_elements_xpath_css_intro/LocatingDropDownList.java
@@ -1,7 +1,9 @@
 package com.cybertek.day04_common_elements_xpath_css_intro;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LocatingDropDownList {
@@ -10,6 +12,13 @@ public class LocatingDropDownList {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("http://practice.cybertekschool.com/dropdown");
+
+        // TODO: identify the first dropdown
+        WebElement firstDropDown = driver.findElement(By.id("dropdown"));
+
+        // TODO: Wrap it up using Select class so we can use easy methods
+
+        // TODO: Select items in 3 different ways
 
     }
 }
