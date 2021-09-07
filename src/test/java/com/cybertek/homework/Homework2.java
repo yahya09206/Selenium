5ed7e6e073c3964ffe183ac27a764987b65acdb6
Yahya <yahyahussein91@gmail.com>
2021-09-06 19:43:43 -0700

    clicking sign in button if title matches

--- src/test/java/com/cybertek/homework/Homework2.java
+++ src/test/java/com/cybertek/homework/Homework2.java
@@ -1,6 +1,7 @@
 package com.cybertek.homework;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -13,7 +14,7 @@ public class Homework2 {
         driver.get("http://zero.webappsecurity.com/");
 
         if (driver.getTitle().equals("Zero - Personal Banking - Loans - Credit Cards")){
-
+            driver.findElement(By.id("signin_button")).click();
         }
     }
 }
