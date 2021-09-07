0cdb6743df044014cf16eef490456dd8b233e2a3
Yahya <yahyahussein91@gmail.com>
2021-09-07 11:56:24 -0700

    forgot to turn on automatic imports

--- src/test/java/com/cybertek/day04_common_elements_xpath_css_intro/FindElementByCssSelector.java
+++ src/test/java/com/cybertek/day04_common_elements_xpath_css_intro/FindElementByCssSelector.java
@@ -1,7 +1,9 @@
 package com.cybertek.day04_common_elements_xpath_css_intro;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FindElementByCssSelector {
@@ -10,6 +12,17 @@ public class FindElementByCssSelector {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("https://search.yahoo.com");
+
+        // TODO: Identify searchbox using css selector
+        // <input type="text" class="sbq" id="yschsp" name="p" value="" role="combobox">
+
+        // try different way, by id, by name
+        WebElement searchbox = driver.findElement(By.cssSelector("#yschsp"));
+        // WebElement searchbox = driver.findElement(By.cssSelector("input[name='p'"));
+        // WebElement searchbox = driver.findElement(By.cssSelector("input[id='yschsp'"));
+        // WebElement searchbox = driver.findElement(By.cssSelector("input[name='p'[type='text'"));
+        searchbox.sendKeys("CSS SELECTOR");
+
 
 
     }
