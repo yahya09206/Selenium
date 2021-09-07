70924552a7801df2f1fb977b4206c88102980f7e
Yahya <yahyahussein91@gmail.com>
2021-09-07 09:21:51 -0700

    drop down list practice

---
+++ src/test/java/com/cybertek/day04_common_elements_xpath_css_intro/LocatingDropDownList.java
@@ -0,0 +1,15 @@
+package com.cybertek.day04_common_elements_xpath_css_intro;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LocatingDropDownList {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://practice.cybertekschool.com/dropdown");
+
+    }
+}
