2a7b465f5f3bebfc0750229cae17fe67eda7a5fa
Yahya <yahyahussein91@gmail.com>
2021-09-07 12:46:41 -0700

    practice with using xpath

---
+++ src/test/java/com/cybertek/day04_common_elements_xpath_css_intro/FindElementByXPath_Intro.java
@@ -0,0 +1,8 @@
+package com.cybertek.day04_common_elements_xpath_css_intro;
+
+public class FindElementByXPath_Intro {
+    public static void main(String[] args) {
+
+        // /html/body/div/div[2]/div/div/div/h1/span
+    }
+}
