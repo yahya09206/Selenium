07af94fe3e09bc8557f59d190183555b2fd6d680
Yahya <yahyahussein91@gmail.com>
2021-09-07 08:45:01 -0700

    lets see if it prints out

--- src/test/java/com/cybertek/homework/Homework2.java
+++ src/test/java/com/cybertek/homework/Homework2.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Homework2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         // Set up Chrome Driver
         WebDriverManager.chromedriver().setup();
@@ -18,6 +18,14 @@ public class Homework2 {
         WebElement searchBox = driver.findElement(By.name("q"));
         searchBox.sendKeys("Selenium");
         driver.findElement(By.name("btnI")).submit();
+
+        // Get text from div that has count of results
+        WebElement countDiv = driver.findElement(By.id("result-stats"));
+        System.out.println("countDiv.getText() = " + countDiv.getText());
+
+
+        Thread.sleep(5000);
+        //driver.quit();
 
     }
 }
