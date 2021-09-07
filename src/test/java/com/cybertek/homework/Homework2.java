40a11b831447bdf86b2757e73bb37a7b39cfc071
Yahya <yahyahussein91@gmail.com>
2021-09-07 08:49:20 -0700

    seeing if i can print first and last index

--- src/test/java/com/cybertek/homework/Homework2.java
+++ src/test/java/com/cybertek/homework/Homework2.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
+
 public class Homework2 {
     public static void main(String[] args) throws InterruptedException {
 
@@ -23,9 +25,16 @@ public class Homework2 {
         WebElement countDiv = driver.findElement(By.id("result-stats"));
         System.out.println("countDiv.getText() = " + countDiv.getText());
 
+        // Get link that contain selenium
+        List<WebElement> containSel = driver.findElements(By.linkText("Selenium"));
+        // Print first
+        System.out.println("containSel.get(0) = " + containSel.get(0));
+        // Print last
+        System.out.println("containSel.get(containSel.size() -1 ) = " + containSel.get(containSel.size() - 1));
 
-        Thread.sleep(5000);
-        //driver.quit();
+
+        Thread.sleep(2000);
+        driver.quit();
 
     }
 }
