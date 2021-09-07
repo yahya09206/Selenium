4255fa93d978d9c67a02d07a706e60577dba71b4
Yahya <yahyahussein91@gmail.com>
2021-09-07 08:37:29 -0700

    navigating to google.com

---
+++ src/test/java/com/cybertek/homework/Homework2.java
@@ -0,0 +1,15 @@
+package com.cybertek.homework;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Homework2 {
+    public static void main(String[] args) {
+
+        // Set up Chrome Driver
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.navigate().to("http://google.com");
+    }
+}
