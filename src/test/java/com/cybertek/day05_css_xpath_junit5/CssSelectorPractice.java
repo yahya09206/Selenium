7955ee728cf3409365696c6b549fb5f0a6db452a
Yahya <yahyahussein91@gmail.com>
2021-09-13 08:36:47 -0700

    navigating to cybertek practice school

--- src/test/java/com/cybertek/day05_css_xpath_junit5/CssSelectorPractice.java
+++ src/test/java/com/cybertek/day05_css_xpath_junit5/CssSelectorPractice.java
@@ -1,7 +1,16 @@
 package com.cybertek.day05_css_xpath_junit5;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class CssSelectorPractice {
     public static void main(String[] args) {
 
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://practice.cybertekschool.com/forgot_password");
+
+        //
     }
 }
