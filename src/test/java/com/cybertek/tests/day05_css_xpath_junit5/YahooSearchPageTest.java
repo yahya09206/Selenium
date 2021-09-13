e0d884121825b508a92a38af63af4710f79ac705
Yahya <yahyahussein91@gmail.com>
2021-09-13 12:42:13 -0700

    instructions for what to test

--- src/test/java/com/cybertek/tests/day05_css_xpath_junit5/YahooSearchPageTest.java
+++ src/test/java/com/cybertek/tests/day05_css_xpath_junit5/YahooSearchPageTest.java
@@ -2,5 +2,13 @@ package com.cybertek.tests.day05_css_xpath_junit5;
 
 public class YahooSearchPageTest {
 
+    // write 2 tests:
+    // navigate to https://search.yahoo.com
+    // test when you navigate to yahoo search page
+        // the title should be "Yahoo Search - Web Search"
+
+    // test when you navigate to yahoo search page
+        // and search for Selenium
+            // Title should start with selenium
 
 }
