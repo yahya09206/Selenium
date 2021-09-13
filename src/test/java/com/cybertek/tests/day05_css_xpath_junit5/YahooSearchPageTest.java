0391c501812d5fad2705aac994dbbc66169c5eca
Yahya <yahyahussein91@gmail.com>
2021-09-13 12:44:50 -0700

    setting up method to test search home page

--- src/test/java/com/cybertek/tests/day05_css_xpath_junit5/YahooSearchPageTest.java
+++ src/test/java/com/cybertek/tests/day05_css_xpath_junit5/YahooSearchPageTest.java
@@ -1,14 +1,22 @@
 package com.cybertek.tests.day05_css_xpath_junit5;
 
+import org.junit.jupiter.api.Test;
+
 public class YahooSearchPageTest {
 
     // write 2 tests:
+    // testYahooSearchHomePageTitle
     // navigate to https://search.yahoo.com
     // test when you navigate to yahoo search page
         // the title should be "Yahoo Search - Web Search"
+    @Test
+    public void testYahooSearchHomePageTitle(){
 
+    }
+    // test method name: testYahooSearchResultPageTitle
     // test when you navigate to yahoo search page
         // and search for Selenium
             // Title should start with selenium
+
 
 }
