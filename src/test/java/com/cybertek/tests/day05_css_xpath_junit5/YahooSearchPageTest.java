b0a9fb94e92e23e5d112ee123a7315bf7dc1fe61
Yahya <yahyahussein91@gmail.com>
2021-09-13 12:46:27 -0700

    maximizing screen

--- src/test/java/com/cybertek/tests/day05_css_xpath_junit5/YahooSearchPageTest.java
+++ src/test/java/com/cybertek/tests/day05_css_xpath_junit5/YahooSearchPageTest.java
@@ -1,6 +1,9 @@
 package com.cybertek.tests.day05_css_xpath_junit5;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class YahooSearchPageTest {
 
@@ -12,6 +15,10 @@ public class YahooSearchPageTest {
     @Test
     public void testYahooSearchHomePageTitle(){
 
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("");
     }
     // test method name: testYahooSearchResultPageTitle
     // test when you navigate to yahoo search page
