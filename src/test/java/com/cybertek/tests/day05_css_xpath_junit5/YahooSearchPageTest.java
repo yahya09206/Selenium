d9f808d2f0cb6fbaa8c279a78322d8005198a39e
Yahya <yahyahussein91@gmail.com>
2021-09-13 12:51:22 -0700

    checking if title matches

--- src/test/java/com/cybertek/tests/day05_css_xpath_junit5/YahooSearchPageTest.java
+++ src/test/java/com/cybertek/tests/day05_css_xpath_junit5/YahooSearchPageTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class YahooSearchPageTest {
 
     // write 2 tests:
@@ -21,6 +23,16 @@ public class YahooSearchPageTest {
         driver.get("https://search.yahoo.com/");
 
         String expectedTitle = "https://search.yahoo.com/";
+        String actualTitle = driver.getTitle();
+
+        // quit browser
+        driver.quit();
+
+        // do static import so you can do this
+        // import static org.junit.jupiter.api.Assertions.*;
+        assertEquals(expectedTitle, actualTitle);
+
+
     }
     // test method name: testYahooSearchResultPageTitle
     // test when you navigate to yahoo search page
