4ecaa1b53c60ad5025f09d2496c02ef272bb3022
Yahya <yahyahussein91@gmail.com>
2021-09-14 09:57:26 -0700

    done with switch statement and returning driver

--- src/test/java/com/cybertek/utility/WebDriverFactory.java
+++ src/test/java/com/cybertek/utility/WebDriverFactory.java
@@ -1,6 +1,9 @@
 package com.cybertek.utility;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class WebDriverFactory {
 
@@ -10,7 +13,19 @@ public class WebDriverFactory {
 
         switch (browserName){
 
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            default:
+                driver = null;
+                System.out.println("UNKNOWN BROWSER TYPE!!! " + browserName);
         }
+        return driver;
 
     }
 }
