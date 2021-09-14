ea1d492ae7ffc84dca564a971b9d5611678e0c07
Yahya <yahyahussein91@gmail.com>
2021-09-14 12:28:18 -0700

    selecting button to click

--- src/test/java/com/cybertek/tests/day06_junit_practice_utility_methods/AlertPracticeTest.java
+++ src/test/java/com/cybertek/tests/day06_junit_practice_utility_methods/AlertPracticeTest.java
@@ -2,6 +2,7 @@ package com.cybertek.tests.day06_junit_practice_utility_methods;
 
 import com.cybertek.utility.TestBase;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
 public class AlertPracticeTest extends TestBase {
 
@@ -14,5 +15,9 @@ public class AlertPracticeTest extends TestBase {
     public void testJavaScriptAlert(){
 
         driver.get("http://practice.cybertekschool.com/javascript_alerts");
+        // Alert interface from Selenium is used to deal with alerts
+        // you driver object can switchTo the alert and take action on it
+
+        driver.findElement(By.xpath("//button[.='Click for JS Alert']")).click();
     }
 }
