ab2fb3a0d5c9001670cda86af53e2bccbff1e2ac
Yahya <yahyahussein91@gmail.com>
2021-09-14 11:59:27 -0700

    done with implicit wait

--- src/test/java/com/cybertek/tests/day06_junit_practice_utility_methods/SlowElementTest.java
+++ src/test/java/com/cybertek/tests/day06_junit_practice_utility_methods/SlowElementTest.java
@@ -5,14 +5,12 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
-import java.util.concurrent.TimeUnit;
-
 public class SlowElementTest extends TestBase {
 
     @Test
     public void testWait() {
 
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         // we can instruct web driver to wait maximum amount of time
         // before throwing NoSuchElement Exception by using implicit wait
         // If element is not available, it will wait for maximum of 10seconds
