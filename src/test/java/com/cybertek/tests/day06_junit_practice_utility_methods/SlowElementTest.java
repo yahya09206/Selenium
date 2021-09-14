463f44dabebc9e7b304c8a29701ae682adba43a1
Yahya <yahyahussein91@gmail.com>
2021-09-14 11:53:45 -0700

    running for 10 seconds

--- src/test/java/com/cybertek/tests/day06_junit_practice_utility_methods/SlowElementTest.java
+++ src/test/java/com/cybertek/tests/day06_junit_practice_utility_methods/SlowElementTest.java
@@ -5,11 +5,14 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
+import java.util.concurrent.TimeUnit;
+
 public class SlowElementTest extends TestBase {
 
     @Test
     public void testWait() throws InterruptedException {
 
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         // we can instruct web driver to wait maximum amount of time
         // before throwing NoSuchElement Exception by using implicit wait
         // If element is not available, it will wait for maximum of 10seconds
@@ -18,7 +21,7 @@ public class SlowElementTest extends TestBase {
         driver.navigate().to("http://practice.cybertekschool.com/dynamic_controls");
         driver.findElement(By.xpath("//button[.='Remove']")).click();
 
-        Thread.sleep(5000);
+        //Thread.sleep(5000);
 
         WebElement itsGoneElm = driver.findElement(By.id("message"));
         System.out.println("itsGoneElm.getText() = " + itsGoneElm.getText());
