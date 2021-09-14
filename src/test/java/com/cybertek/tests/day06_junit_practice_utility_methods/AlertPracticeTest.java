1d7a16787ef22926e89b215dd417ad6b96689c2c
Yahya <yahyahussein91@gmail.com>
2021-09-14 12:34:44 -0700

    select alert is working

--- src/test/java/com/cybertek/tests/day06_junit_practice_utility_methods/AlertPracticeTest.java
+++ src/test/java/com/cybertek/tests/day06_junit_practice_utility_methods/AlertPracticeTest.java
@@ -19,5 +19,19 @@ public class AlertPracticeTest extends TestBase {
         // you driver object can switchTo the alert and take action on it
 
         driver.findElement(By.xpath("//button[.='Click for JS Alert']")).click();
+        // driver first needs to switch to the alert box since it's not part of html
+        // using switchTo() method followed by alert() method
+        // .alert() method return a type Alert from Selenium
+        // and it has 4 methods :
+            // - accept() -->> click ok button
+            // - dismiss() -->> click on cancel button
+            // - sendKey(yourText) -->> enter text into prompt box
+        driver.switchTo().alert().accept();
+//        Alert alertObj = driver.switchTo().alert();
+//        alertObj.accept();
+
+
+
+        System.out.println("The end");
     }
 }
