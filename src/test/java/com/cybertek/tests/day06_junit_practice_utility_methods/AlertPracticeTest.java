051bfcbd15465cdeccbac6edcdcd63483035dbc1
Yahya <yahyahussein91@gmail.com>
2021-09-14 12:22:12 -0700

    practice with javascript alert

---
+++ src/test/java/com/cybertek/tests/day06_junit_practice_utility_methods/AlertPracticeTest.java
@@ -0,0 +1,12 @@
+package com.cybertek.tests.day06_junit_practice_utility_methods;
+
+import com.cybertek.utility.TestBase;
+import org.junit.jupiter.api.Test;
+
+public class AlertPracticeTest extends TestBase {
+
+    @Test
+    public void testJavaScriptAlert(){
+
+    }
+}
