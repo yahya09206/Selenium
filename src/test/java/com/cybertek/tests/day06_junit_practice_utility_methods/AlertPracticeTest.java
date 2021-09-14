076261fb94a7dbe66996551700a1f638baf92d22
Yahya <yahyahussein91@gmail.com>
2021-09-14 12:24:48 -0700

    updated comments

--- src/test/java/com/cybertek/tests/day06_junit_practice_utility_methods/AlertPracticeTest.java
+++ src/test/java/com/cybertek/tests/day06_junit_practice_utility_methods/AlertPracticeTest.java
@@ -5,7 +5,11 @@ import org.junit.jupiter.api.Test;
 
 public class AlertPracticeTest extends TestBase {
 
+    // Javascript alerts are pop up that show up after you click on button
+    // in certain pages, and there are 3 types of pop-up
     // regular(ok), confirm(ok and cancel), prompt(enter text)
+    // confirm(ok and cancel)
+    // prompt(enter text) and select ok and cancel
     @Test
     public void testJavaScriptAlert(){
 
