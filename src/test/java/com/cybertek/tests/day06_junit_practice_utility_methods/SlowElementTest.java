2a58a8218bc5755624e0b1466bc021af3f9f56a7
Yahya <yahyahussein91@gmail.com>
2021-09-14 11:48:23 -0700

    5 second wait is passing

--- src/test/java/com/cybertek/tests/day06_junit_practice_utility_methods/SlowElementTest.java
+++ src/test/java/com/cybertek/tests/day06_junit_practice_utility_methods/SlowElementTest.java
@@ -8,12 +8,15 @@ import org.openqa.selenium.WebElement;
 public class SlowElementTest extends TestBase {
 
     @Test
-    public void testWait(){
+    public void testWait() throws InterruptedException {
 
         driver.navigate().to("http://practice.cybertekschool.com/dynamic_controls");
         driver.findElement(By.xpath("//button[.='Remove']")).click();
 
+        Thread.sleep(5000);
+
         WebElement itsGoneElm = driver.findElement(By.id("message"));
         System.out.println("itsGoneElm.getText() = " + itsGoneElm.getText());
+        System.out.println("THE END");
     }
 }
