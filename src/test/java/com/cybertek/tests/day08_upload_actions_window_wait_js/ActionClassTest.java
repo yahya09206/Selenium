e782805de8ba8616c5c10b0ce596e446e2d803c9
Yahya <yahyahussein91@gmail.com>
2021-09-20 08:51:56 -0700

    had to run in debug mode

--- src/test/java/com/cybertek/tests/day08_upload_actions_window_wait_js/ActionClassTest.java
+++ src/test/java/com/cybertek/tests/day08_upload_actions_window_wait_js/ActionClassTest.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class ActionClassTest extends TestBase {
 
     @Test
@@ -23,6 +25,16 @@ public class ActionClassTest extends TestBase {
         // Create instance of Action class by passing driver object
         Actions actions = new Actions(driver);
         actions.moveToElement(img1Elm).perform();
+
+        BrowserUtil.waitFor(3);
+
+        // and verify User Profile 1 is displayed with text "name: user1"
+        WebElement firstProfileElm = driver.findElement(By.xpath("//h5[.='name: user1']"));
+        assertTrue(firstProfileElm.isDisplayed());
+
+        // locate second image and hover over to the image and verify user profile 1 displayed
+        WebElement img2Elm = driver.findElement(By.xpath("(//div[@class='figure']/img)[2]"));
+        actions.moveToElement(img2Elm).perform();
 
         BrowserUtil.waitFor(3);
     }
