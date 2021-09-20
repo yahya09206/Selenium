e69ba6127777055f4a537e50162d1c7bdd7bb7dd
Yahya <yahyahussein91@gmail.com>
2021-09-20 07:31:46 -0700

    uploading file by finding id

--- src/test/java/com/cybertek/tests/day08_upload_actions_window_wait_js/FileUploadTest.java
+++ src/test/java/com/cybertek/tests/day08_upload_actions_window_wait_js/FileUploadTest.java
@@ -1,7 +1,26 @@
 package com.cybertek.tests.day08_upload_actions_window_wait_js;
 
-public class FileUploadTest {
-    public static void main(String[] args) {
+import com.cybertek.utility.TestBase;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
+public class FileUploadTest extends TestBase {
+
+    @Test
+    public void testUpload(){
+
+        driver.get("http://practice.cybertekschool.com/upload");
+
+        // locate the input field : By.id("file-upload)
+        /**
+         * By.id("file-upload)
+         * By.xpath("//input[@id='file-upload']")
+         * By.cssSelector("input[id=file-upload']") Handle #file-upload
+         **/
+
+        WebElement fileInputField = driver.findElement(By.id("file-upload"));
+        // find the full path of your file in your computer and send key into above input field
+        String filePath = "/Users/solodolo/Desktop/Screen Shot 2021-08-16 at 12.40.46 PM.png";
     }
 }
