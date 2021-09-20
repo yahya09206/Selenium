f5d937fe0a6b647952fa0a3d2c2007a898c41f44
Yahya <yahyahussein91@gmail.com>
2021-09-20 08:10:46 -0700

    surround with try catch

--- src/test/java/com/cybertek/utility/BrowserUtil.java
+++ src/test/java/com/cybertek/utility/BrowserUtil.java
@@ -9,6 +9,10 @@ public class BrowserUtil {
 
     public static void waitFor(int seconds){
 
-
+        try {
+            Thread.sleep(seconds * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
