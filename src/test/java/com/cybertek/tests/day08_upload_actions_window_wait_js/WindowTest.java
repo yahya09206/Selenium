00b0016004b10a7cdedbb67f950fe797a4ee4648
Yahya <yahyahussein91@gmail.com>
2021-09-20 12:40:42 -0700

    asserting for title

--- src/test/java/com/cybertek/tests/day08_upload_actions_window_wait_js/WindowTest.java
+++ src/test/java/com/cybertek/tests/day08_upload_actions_window_wait_js/WindowTest.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.WebElement;
 
 import java.util.Set;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class WindowTest extends TestBase {
 
     @Test
@@ -31,6 +33,33 @@ public class WindowTest extends TestBase {
         // now print out current and all window handles
         System.out.println("driver.getWindowHandle() = " + driver.getWindowHandle());
         // print out all window handles
+        // after you click now you have 2 tabs
+        // in order to get all of them, you can call driver.getWindowHandles()
+        // and we can print it out
         System.out.println("driver.getWindowHandles() = " + driver.getWindowHandles());
+
+        // our objective is to iterate over each and every handle
+        // and use switch method to switch to the window
+        // print out its title
+        // this is reassigning the variable to new handles
+        allHandles = driver.getWindowHandles(); // this will give the latest handles
+
+        // iterate over Collection, you can use for each loop
+        for(String eachHandle : allHandles){
+            System.out.println("eachHandle = " + eachHandle);
+            // this is how you switch to different window
+            driver.switchTo().window(eachHandle);
+            // now we can print out the title so we know what tab we are in right now
+            System.out.println("driver.getTitle() = " + driver.getTitle());
+        }
+
+        // at the end of execution we observed
+        // last switched window was window with the title "New Window"
+        // now we want to go back to original tab so we can continue to work on it
+        System.out.println("Switching back to original tab");
+        driver.switchTo().window(currentHandle);
+        // assert the title is now "Practice"
+        assertEquals("Practice", driver.getTitle());
+
     }
 }
