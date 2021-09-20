c23dbb382341de296634518e24c3e6bf2704d921
Yahya <yahyahussein91@gmail.com>
2021-09-20 08:09:54 -0700

    method to replace thread sleep

---
+++ src/test/java/com/cybertek/utility/BrowserUtil.java
@@ -0,0 +1,14 @@
+package com.cybertek.utility;
+
+public class BrowserUtil {
+
+    /**
+     * A method to pause the thread a specified amount of time
+     * @param seconds
+     */
+
+    public static void waitFor(int seconds){
+
+
+    }
+}
