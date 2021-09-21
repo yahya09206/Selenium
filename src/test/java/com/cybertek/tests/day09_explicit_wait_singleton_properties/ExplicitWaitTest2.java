187db17f2779d4d7176dec205fb17b3b5297afe4
Yahya <yahyahussein91@gmail.com>
2021-09-21 09:45:37 -0700

    test is passing

--- src/test/java/com/cybertek/tests/day09_explicit_wait_singleton_properties/ExplicitWaitTest2.java
+++ src/test/java/com/cybertek/tests/day09_explicit_wait_singleton_properties/ExplicitWaitTest2.java
@@ -1,27 +1,38 @@
 package com.cybertek.tests.day09_explicit_wait_singleton_properties;
 
+import com.cybertek.utility.BrowserUtil;
 import com.cybertek.utility.TestBase;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import static org.openqa.selenium.support.ui.ExpectedConditions.*;
+import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
 public class ExplicitWaitTest2 extends TestBase {
 
     @Test
     public void waitUntilElementInteractive(){
 
-        driver.get("http://practice.cybertek.com/dynamic_controls");
+        driver.get("http://practice.cybertekschool.com/dynamic_controls");
 
         // elementToBeClickable(By locator)
         // elementToBeClickable(WebElement element)
         // An exception for checking an element is visible and enabled such that you can click it
 
-        // click on enable button and wait until input=box is enabled clickable
+        WebElement inputField = driver.findElement(By.cssSelector("form#input-example>input"));
+        System.out.println("inputField.getText() = " + inputField.getText());
 
+        // click on enable button and wait until input=box is enabled clickable
         driver.findElement(By.xpath("//button[.='Enable']")).click();
 
+
         WebDriverWait wait = new WebDriverWait(driver, 7);
-        wait.until(elementToBeClickable(By.cssSelector("form#input-example>input")));
+        // wait.until(elementToBeClickable(By.cssSelector("form#input-example>input")));
+        wait.until(elementToBeClickable(inputField));
+        // now enter something into the input field
+        // now enter something into the input filed
+        inputField.sendKeys("HELLO WORLD");
+
+        BrowserUtil.waitFor(5);
     }
 }
