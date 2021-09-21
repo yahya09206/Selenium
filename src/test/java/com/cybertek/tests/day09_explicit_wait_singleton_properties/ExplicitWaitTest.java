869e3112e718ee497d89aec0019870a4fd4d2530
Yahya <yahyahussein91@gmail.com>
2021-09-21 08:26:29 -0700

    day 9 explicit wait and singleton properties

---
+++ src/test/java/com/cybertek/tests/day09_explicit_wait_singleton_properties/ExplicitWaitTest.java
@@ -0,0 +1,12 @@
+package com.cybertek.tests.day09_explicit_wait_singleton_properties;
+
+import com.cybertek.utility.TestBase;
+import org.junit.jupiter.api.Test;
+
+public class ExplicitWaitTest extends TestBase {
+
+    @Test
+    public void testWaitForTitle(){
+
+    }
+}
