526fb7bfc60e4800d01d841f47c4fea01ef0605d
Yahya <yahyahussein91@gmail.com>
2021-09-24 22:21:06 -0700

    fixed without using import

--- src/test/java/com/cybertek/tests/day09_explicit_wait_singleton_properties/WebOrderTest.java
+++ src/test/java/com/cybertek/tests/day09_explicit_wait_singleton_properties/WebOrderTest.java
@@ -4,15 +4,13 @@ import com.cybertek.utility.BrowserUtil;
 import com.cybertek.utility.WebOrderUtility;
 import org.junit.jupiter.api.Test;
 
-import static com.cybertek.utility.WebOrderUtility.openWebOrderApp;
-
 public class WebOrderTest {
 
     @Test
     public void testLogin(){
 
         // using new driver from Driver class to navigate to login page
-       openWebOrderApp();
+        WebOrderUtility.openWebOrderApp();
 
         WebOrderUtility.login("Tester", "test");
         BrowserUtil.waitFor(2);
