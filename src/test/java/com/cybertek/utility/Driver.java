05ee6c23f66a2d4646d36612f882ab22d46b3f9f
Yahya <yahyahussein91@gmail.com>
2021-09-24 21:18:02 -0700

    set up private field and constructor

--- src/test/java/com/cybertek/utility/Driver.java
+++ src/test/java/com/cybertek/utility/Driver.java
@@ -1,4 +1,16 @@
 package com.cybertek.utility;
 
+import org.openqa.selenium.WebDriver;
+
 public class Driver {
+
+    // private static field
+    private static WebDriver obj;
+    // private constructor
+    private Driver(){ };
+
+    // getter with conditional object creation
+    public static WebDriver getDriver(){
+
+    }
 }
