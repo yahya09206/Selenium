1951c2787b54804bf76be28206dfff7b83a167c4
Yahya <yahyahussein91@gmail.com>
2021-09-26 14:43:10 -0700

    headers are matching

--- src/test/java/com/cybertek/tests/day09_explicit_wait_singleton_properties/WebOrderFunctionalityTests.java
+++ src/test/java/com/cybertek/tests/day09_explicit_wait_singleton_properties/WebOrderFunctionalityTests.java
@@ -1,10 +1,13 @@
 package com.cybertek.tests.day09_explicit_wait_singleton_properties;
 
+import com.cybertek.utility.Driver;
 import com.cybertek.utility.WebOrderUtility;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class WebOrderFunctionalityTests {
@@ -35,6 +38,10 @@ public class WebOrderFunctionalityTests {
     @Test
     public void testAllProductNames(){
         WebOrderUtility.selectSideBarTab("View all products");
+
+        String actualHeader = Driver.getDriver().findElement(By.xpath("//h2")).getText();
+        String expectedHeader = "List of Products";
+        assertEquals(actualHeader, expectedHeader);
 
     }
 }
