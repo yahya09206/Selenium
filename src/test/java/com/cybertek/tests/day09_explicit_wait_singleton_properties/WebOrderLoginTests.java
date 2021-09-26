694c5d16fddcd5a264c2feb6aeca550771b144b6
Yahya <yahyahussein91@gmail.com>
2021-09-25 22:15:16 -0700

    verifying on correct page

--- src/test/java/com/cybertek/tests/day09_explicit_wait_singleton_properties/WebOrderLoginTests.java
+++ src/test/java/com/cybertek/tests/day09_explicit_wait_singleton_properties/WebOrderLoginTests.java
@@ -1,5 +1,6 @@
 package com.cybertek.tests.day09_explicit_wait_singleton_properties;
 
+import com.cybertek.utility.Driver;
 import com.cybertek.utility.WebOrderUtility;
 import org.junit.jupiter.api.Test;
 
@@ -8,7 +9,17 @@ public class WebOrderLoginTests {
     @Test
     public void testLoginWithCorrectCredentials(){
 
+        // go to login page
         WebOrderUtility.openWebOrderApp();
+        // login with correct credentials
+        WebOrderUtility.login("Tester", "test");
+
+        // get title
+        String pageTitle = Driver.getDriver().getTitle();
+        // check if at all orders page
+        if (Driver.getDriver().getTitle().equals("Web Orders")){
+            System.out.println("You are on the correct page " + pageTitle);
+        }
     }
 
 }
