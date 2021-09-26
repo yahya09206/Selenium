493ad7873a151b0896793747078fabbceaa0e1b8
Yahya <yahyahussein91@gmail.com>
2021-09-26 14:09:38 -0700

    method to test if all buttons are checked

--- src/test/java/com/cybertek/tests/day09_explicit_wait_singleton_properties/WebOrderFunctionalityTests.java
+++ src/test/java/com/cybertek/tests/day09_explicit_wait_singleton_properties/WebOrderFunctionalityTests.java
@@ -1,5 +1,11 @@
 package com.cybertek.tests.day09_explicit_wait_singleton_properties;
 
+import org.junit.jupiter.api.Test;
+
 public class WebOrderFunctionalityTests {
 
+    @Test
+    public void testCheckAllButton(){
+
+    }
 }
