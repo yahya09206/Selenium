744330c00c17fc0432ae45fa668452fba06a5bd0
Yahya <yahyahussein91@gmail.com>
2021-09-26 14:11:21 -0700

    added annotations

--- src/test/java/com/cybertek/tests/day09_explicit_wait_singleton_properties/WebOrderFunctionalityTests.java
+++ src/test/java/com/cybertek/tests/day09_explicit_wait_singleton_properties/WebOrderFunctionalityTests.java
@@ -1,11 +1,28 @@
 package com.cybertek.tests.day09_explicit_wait_singleton_properties;
 
+import com.cybertek.utility.WebOrderUtility;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class WebOrderFunctionalityTests {
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+public class WebOrderFunctionalityTests {
+    @BeforeEach
+    public void setUpAndLogin(){
+        WebOrderUtility.openWebOrderApp();
+        WebOrderUtility.login();
+
+    }
+
+    @AfterEach
+    public void quit(){
+        WebOrderUtility.logout();
+
+    }
     @Test
     public void testCheckAllButton(){
 
+        assertTrue(WebOrderUtility.checkAll());
     }
 }
