e6926591fb502e22b44236b722d9cbbe6cea9f04
Yahya <yahyahussein91@gmail.com>
2021-09-25 22:07:56 -0700

    test method for correct login

--- src/test/java/com/cybertek/tests/day09_explicit_wait_singleton_properties/WebOrderLoginTests.java
+++ src/test/java/com/cybertek/tests/day09_explicit_wait_singleton_properties/WebOrderLoginTests.java
@@ -1,6 +1,12 @@
 package com.cybertek.tests.day09_explicit_wait_singleton_properties;
 
+import org.junit.jupiter.api.Test;
+
 public class WebOrderLoginTests {
 
+    @Test
+    public void testLoginWithCorrectCredentials(){
+
+    }
 
 }
