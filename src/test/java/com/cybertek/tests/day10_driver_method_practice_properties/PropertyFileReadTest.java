8282cad85d36b6d1660f31482a1f0013733054e1
Yahya <yahyahussein91@gmail.com>
2021-09-27 12:24:53 -0700

    load property in inputstream

--- src/test/java/com/cybertek/tests/day10_driver_method_practice_properties/PropertyFileReadTest.java
+++ src/test/java/com/cybertek/tests/day10_driver_method_practice_properties/PropertyFileReadTest.java
@@ -1,4 +1,22 @@
 package com.cybertek.tests.day10_driver_method_practice_properties;
 
+import org.junit.jupiter.api.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class PropertyFileReadTest {
+
+    @Test
+    public void testInitialRead() throws IOException {
+
+        // Open a connection to the file using FileInputStream object
+        FileInputStream in = new FileInputStream("config.properties");
+
+        // Create empty properties object
+        Properties myProperty = new Properties();
+        // Load the FileInputStream into the properties object
+        myProperty.load(in);
+    }
 }
