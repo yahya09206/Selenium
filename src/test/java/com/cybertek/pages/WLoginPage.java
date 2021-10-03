3fe173ce4bd9a780a2e8c6e555731f86a893d5e4
Yahya <yahyahussein91@gmail.com>
2021-10-03 15:38:37 -0700

    done with findby now need to tell selenium to actually find

--- src/test/java/com/cybertek/pages/WLoginPage.java
+++ src/test/java/com/cybertek/pages/WLoginPage.java
@@ -5,8 +5,16 @@ import org.openqa.selenium.support.FindBy;
 
 public class WLoginPage {
 
-    @FindBy()
+    @FindBy(id = "ctl100_MainContent_username")
     public WebElement userNameField;
+
+    @FindBy(name = "ctl100$MainContent$password")
     public WebElement passwordField;
+
+    @FindBy(css = "#ctl100_MainContent_login_button")
     public WebElement loginButton;
+
+    // Now we need instruct selenium to start looking for element
+    // when this constructor is called
+
 }
