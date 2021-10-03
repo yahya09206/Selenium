2aeca20543cd0ed7fe93c0058d322b39973f81ba
Yahya <yahyahussein91@gmail.com>
2021-10-03 15:41:21 -0700

    using page factory class

--- src/test/java/com/cybertek/pages/WLoginPage.java
+++ src/test/java/com/cybertek/pages/WLoginPage.java
@@ -1,7 +1,9 @@
 package com.cybertek.pages;
 
+import com.cybertek.utility.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class WLoginPage {
 
@@ -16,5 +18,12 @@ public class WLoginPage {
 
     // Now we need instruct selenium to start looking for element
     // when this constructor is called
-
+    public WLoginPage(){
+        // PageFactory is a selenium class that supports POM, and it has a method initElements
+        // once it's called, it will locate all the elements
+        // specified using @FindBy annotation with locator
+        // initElements accept 2 arguments
+        // WebDriver instance and Page class instance (this) means current instance of this class
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
 }
