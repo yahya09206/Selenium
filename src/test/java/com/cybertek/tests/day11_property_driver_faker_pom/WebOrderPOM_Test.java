14b0083e7d85cccbf81ddc3592e34fd0efc1c19f
Yahya <yahyahussein91@gmail.com>
2021-10-03 16:02:10 -0700

    test if failing atm

--- src/test/java/com/cybertek/tests/day11_property_driver_faker_pom/WebOrderPOM_Test.java
+++ src/test/java/com/cybertek/tests/day11_property_driver_faker_pom/WebOrderPOM_Test.java
@@ -2,7 +2,6 @@ package com.cybertek.tests.day11_property_driver_faker_pom;
 
 import com.cybertek.pages.WLoginPage;
 import com.cybertek.utility.BrowserUtil;
-import com.cybertek.utility.Driver;
 import com.cybertek.utility.TestBase;
 import org.junit.jupiter.api.Test;
 
@@ -11,14 +10,14 @@ public class WebOrderPOM_Test extends TestBase {
     @Test
     public void testWithPOM_for_login(){
 
-        Driver.getDriver().get("http://secure.smartbearsoftware.com/samples/TestComplete11/WebOrders/login.aspx");
+        //Driver.getDriver().get("http://secure.smartbearsoftware.com/samples/TestComplete11/WebOrders/login.aspx");
         WLoginPage loginPage = new WLoginPage();
 
         loginPage.goTo();
 //        loginPage.userNameField.sendKeys("Tester");
 //        loginPage.passwordField.sendKeys("test");
+        //loginPage.loginButton.click();
         loginPage.login("Tester", "test");
-        loginPage.loginButton.click();
 
         BrowserUtil.waitFor(4);
     }
