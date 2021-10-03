8b802e52600ff378ab8f9da02876f0c87c2a6a04
Yahya <yahyahussein91@gmail.com>
2021-10-03 15:09:56 -0700

    faker practice

--- src/test/java/com/cybertek/tests/day11_property_driver_faker_pom/FakerPractice.java
+++ src/test/java/com/cybertek/tests/day11_property_driver_faker_pom/FakerPractice.java
@@ -1,4 +1,15 @@
 package com.cybertek.tests.day11_property_driver_faker_pom;
 
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.Test;
+
 public class FakerPractice {
+
+    @Test
+    public void testPrintFakeData(){
+
+        Faker faker = new Faker();
+
+
+    }
 }
