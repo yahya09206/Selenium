63863b2434e30197f96feaaa8048fdc628ecb877
Yahya <yahyahussein91@gmail.com>
2021-10-03 17:14:25 -0700

    pom file for all order page set up

--- src/test/java/com/cybertek/pages/WAllOrderPage.java
+++ src/test/java/com/cybertek/pages/WAllOrderPage.java
@@ -1,6 +1,16 @@
 package com.cybertek.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class WAllOrderPage {
+
+    @FindBy
+    public WebElement header;
+
+    public WebElement checkAllButton;
+
+    public WebElement unCheckAllButton;
 
 
 }
