d45d968c6e38d1858748813a796a4878d71632d4
Yahya <yahyahussein91@gmail.com>
2021-10-03 17:50:34 -0700

    elements to find on web order page

---
+++ src/test/java/com/cybertek/pages/WOrderPage.java
@@ -0,0 +1,21 @@
+package com.cybertek.pages;
+
+import org.openqa.selenium.WebElement;
+
+public class WOrderPage {
+
+    public WebElement header;
+
+    public WebElement productDropdown;
+
+    public WebElement quantityBox;
+
+    public WebElement priceBox;
+
+    public WebElement discountBox;
+
+    public WebElement totalBox;
+
+    public WebElement calculateButton;
+
+}
