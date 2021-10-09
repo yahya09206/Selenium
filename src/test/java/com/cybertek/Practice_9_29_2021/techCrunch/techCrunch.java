286ae12ba732917b764c471f2d31061d0b9f7abe
Yahya <yahyahussein91@gmail.com>
2021-10-08 21:16:30 -0700

    driver to go to techcrunch website

--- src/test/java/com/cybertek/Practice_9_29_2021/techCrunch/techCrunch.java
+++ src/test/java/com/cybertek/Practice_9_29_2021/techCrunch/techCrunch.java
@@ -1,6 +1,15 @@
 package com.cybertek.Practice_9_29_2021.techCrunch;
 
+import com.cybertek.utility.ConfigReader;
+import com.cybertek.utility.Driver;
 import com.cybertek.utility.TestBase;
+import org.junit.jupiter.api.Test;
 
 public class techCrunch extends TestBase {
+
+    @Test
+    public void testCrunch(){
+
+        Driver.getDriver().get(ConfigReader.read("tech_crunch"));
+    }
 }
