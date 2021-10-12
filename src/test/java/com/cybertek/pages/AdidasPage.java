b9c753ac2632fc21e6dcc1cb620c24a22515a992
Yahya <yahyahussein91@gmail.com>
2021-10-11 21:09:00 -0700

    init elements with constructor

--- src/test/java/com/cybertek/pages/AdidasPage.java
+++ src/test/java/com/cybertek/pages/AdidasPage.java
@@ -1,4 +1,11 @@
 package com.cybertek.pages;
 
+import com.cybertek.utility.Driver;
+import org.openqa.selenium.support.PageFactory;
+
 public class AdidasPage {
+
+    public AdidasPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
 }
