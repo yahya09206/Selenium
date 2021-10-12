c677cc778f3a7db68f3b530e2339cdc8caa67457
Yahya <yahyahussein91@gmail.com>
2021-10-11 21:09:55 -0700

    re added everything that i deleted

--- src/test/java/com/cybertek/Practice_9_29_2021/adidasTask/Adidas.java
+++ src/test/java/com/cybertek/Practice_9_29_2021/adidasTask/Adidas.java
@@ -1,5 +1,15 @@
 package com.cybertek.Practice_9_29_2021.adidasTask;
 
-public class Adidas  {
+import com.cybertek.utility.ConfigReader;
+import com.cybertek.utility.Driver;
+import com.cybertek.utility.TestBase;
+import org.junit.jupiter.api.Test;
 
+public class Adidas extends TestBase {
+
+    @Test
+    public void adidas(){
+
+        Driver.getDriver().get(ConfigReader.read("adidas"));
+    }
 }
