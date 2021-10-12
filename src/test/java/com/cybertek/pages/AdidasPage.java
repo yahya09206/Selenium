a6b1222ba7c1fb3301b5bae7246623dd1ca84c32
Yahya <yahyahussein91@gmail.com>
2021-10-11 21:15:53 -0700

    using findby id to locate element

--- src/test/java/com/cybertek/pages/AdidasPage.java
+++ src/test/java/com/cybertek/pages/AdidasPage.java
@@ -1,6 +1,8 @@
 package com.cybertek.pages;
 
 import com.cybertek.utility.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class AdidasPage {
@@ -8,4 +10,7 @@ public class AdidasPage {
     public AdidasPage() {
         PageFactory.initElements(Driver.getDriver(), this);
     }
+
+    @FindBy(xpath = "//h3[@class='price-container']")
+    WebElement
 }
