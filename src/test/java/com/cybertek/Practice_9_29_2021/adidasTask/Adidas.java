8fb3d735b8e3b4631f8b47568a7cb43ba4aa7f2e
Yahya <yahyahussein91@gmail.com>
2021-10-11 21:08:07 -0700

    new page for adidas site

--- src/test/java/com/cybertek/Practice_9_29_2021/adidasTask/Adidas.java
+++ src/test/java/com/cybertek/Practice_9_29_2021/adidasTask/Adidas.java
@@ -1,15 +1,5 @@
 package com.cybertek.Practice_9_29_2021.adidasTask;
 
-import com.cybertek.utility.ConfigReader;
-import com.cybertek.utility.Driver;
-import com.cybertek.utility.TestBase;
-import org.junit.jupiter.api.Test;
+public class Adidas  {
 
-public class Adidas extends TestBase {
-
-    @Test
-    public void adidas(){
-
-        Driver.getDriver().get(ConfigReader.read("adidas"));
-    }
 }
